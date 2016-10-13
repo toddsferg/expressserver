c0f8c3e0564575dca1bc01ba901a0f37bae98f8b
Todd Ferguson <toddsferg@gmail.com>
2016-10-13 20:39:35 +0000

    initial commit of urlshortener

---
+++ express_server.js
@@ -0,0 +1,44 @@
+var express = require("express");
+var app = express();
+app.set("view engine", "ejs");
+var PORT = process.env.PORT || 8080; // default port 8080
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+// app.get("/", (req, res) => {
+//   res.end("Hello!");
+// });
+
+app.get("/urls", (req, res) => {
+  var templateVars = { urls: urlDatabase };
+  res.render("urls_index.ejs", templateVars);
+});
+
+
+
+
+// app.get("/urls", (req,res) => {
+//   res.render('urls_index.ejs', {
+//     urlDatabase: "Smoke"
+//   });
+// });
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.end("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls", (req, res) => {
+  var templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
