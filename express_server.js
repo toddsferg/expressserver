3b3fb47c5b2795957cd4a8a3de0dee9bce6b47d7
Todd Ferguson <toddsferg@gmail.com>
2016-10-14 04:42:47 +0000

    second commit, url shortener looking promising

--- express_server.js
+++ express_server.js
@@ -1,11 +1,26 @@
+//dependancies
+
 var express = require("express");
+var bodyParser = require("body-parser");
+
+// setting app to express
 var app = express();
+
+//configuration
+
+app.set('port', process.env.PORT || 8080); // default port 8080
 app.set("view engine", "ejs");
-var PORT = process.env.PORT || 8080; // default port 8080
+
+//middleware
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
+  "8JLaoC": "http://www.tsn.ca"
 };
 
 // app.get("/", (req, res) => {
@@ -14,31 +29,34 @@ var urlDatabase = {
 
 app.get("/urls", (req, res) => {
   var templateVars = { urls: urlDatabase };
-  res.render("urls_index.ejs", templateVars);
+  res.render("urls_show.ejs", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  var templateVars = { urls: urlDatabase };
+  res.render("urls_new.ejs", templateVars);
+});
 
-
-
-// app.get("/urls", (req,res) => {
-//   res.render('urls_index.ejs', {
-//     urlDatabase: "Smoke"
-//   });
+// app.get("/urls/:id", (req, res) => {
+//   var templateVars = { shortURL: req.params.id };
+//   res.render("urls_show.ejs", templateVars);
 // });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+app.post("/urls", (req, res) => {
+  console.log(urlDatabase[generateRandomString()] = req.body.longURL);  // debug statement to see POST parameters
+  res.redirect("/urls/new");         // Respond with 'Ok' (we will replace this)
 });
 
-app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n");
+app.listen(app.get('port'), () => {
+  console.log(`Example app listening on port ${app.get('port')}!`);
 });
 
-app.get("/urls", (req, res) => {
-  var templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
+var generateRandomString = function(){
+  var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var output = "";
+for (var i = 0; i < 6; i++) {
+   output += characters.charAt(Math.floor(Math.random() * characters.length))
+} return output;
+}
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
+console.log(generateRandomString());
