5c44f3ebbc90a575239d4bf27b8e2bce467fd6e4
Todd Ferguson <toddsferg@gmail.com>
2016-11-06 15:03:15 +0000

    tinyapp with cookies

--- express_server.js
+++ express_server.js
@@ -1,62 +1,120 @@
-//dependancies
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const PORT = process.env.PORT || 8080;
+const app = express();
 
-var express = require("express");
-var bodyParser = require("body-parser");
-
-// setting app to express
-var app = express();
-
-//configuration
-
-app.set('port', process.env.PORT || 8080); // default port 8080
-app.set("view engine", "ejs");
-
-//middleware
-
+//Middleware
+app.set("view engine", "ejs"); //templates for ejs
 app.use(bodyParser.urlencoded({
   extended: false
-}));
+})); //reads req body
+ app.use(cookieParser());
 
-var urlDatabase = {
+
+const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com",
-  "8JLaoC": "http://www.tsn.ca"
+  "9sm5xK": "http://www.google.com"
 };
 
-// app.get("/", (req, res) => {
-//   res.end("Hello!");
+
+// get home page
+app.get("/", (req, res) => {
+   let templateVars = {
+    username: req.cookies.username,
+     };
+     console.log(templateVars.username);
+  res.render("home", templateVars);
+});
+
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
 // });
 
+// Send user to urls
 app.get("/urls", (req, res) => {
-  var templateVars = { urls: urlDatabase };
-  res.render("urls_show.ejs", templateVars);
+  let templateVars = {
+  urls: urlDatabase,
+  username: req.cookies.username
+  };
+
+  res.render("urls_index", templateVars);
 });
+
+//post info to /urls
+app.post("/urls", (req, res) => {
+  console.log(req.body.longURL);
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect("/urls/" + shortURL);
+});
+
+//go to urls_new(our URLs)
 
 app.get("/urls/new", (req, res) => {
-  var templateVars = { urls: urlDatabase };
-  res.render("urls_new.ejs", templateVars);
+  let templateVars = {
+    username: req.cookies.username
+  };
+  res.render("urls_new", templateVars);
 });
 
-// app.get("/urls/:id", (req, res) => {
-//   var templateVars = { shortURL: req.params.id };
-//   res.render("urls_show.ejs", templateVars);
-// });
-
-app.post("/urls", (req, res) => {
-  console.log(urlDatabase[generateRandomString()] = req.body.longURL);  // debug statement to see POST parameters
-  res.redirect("/urls/new");         // Respond with 'Ok' (we will replace this)
+app.get("/urls/:id", (req, res) => {
+  let longURL = urlDatabase[req.params.id];
+  console.log(longURL);
+  let templateVars = {
+    username: req.cookies.username,
+    shortURL: req.params.id,
+    longURL: longURL,
+    urlDatabase: urlDatabase
+  };
+  res.render("urls_show", templateVars);
 });
 
-app.listen(app.get('port'), () => {
-  console.log(`Example app listening on port ${app.get('port')}!`);
+
+
+app.post("/urls/:id", (req, res) => {
+  let shortURL = req.params.id;
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect("/urls/" + shortURL);
 });
 
-var generateRandomString = function(){
-  var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+
+app.get("/u/:id", (req, res) => {
+  let longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
+});
+//delete using delete operator
+app.post("/urls/:id/delete", (req, res) => {
+  delete urlDatabase[req.params.id];
+  res.redirect("/urls");
+});
+
+
+//accept a POST to /login
+app.post("/login", (req, res) => {
+  console.log("logged in a username")
+  res.cookie("username", req.body.username);
+  res.redirect("/");
+})
+//post to /logout, redirect home
+app.post("/logout", (req, res) => {
+  console.log("logged out")
+  res.clearCookie("username");
+  res.redirect("/");
+})
+
+//generate shortURL
+
+function generateRandomString() {
+   var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   var output = "";
 for (var i = 0; i < 6; i++) {
    output += characters.charAt(Math.floor(Math.random() * characters.length))
 } return output;
-}
+};
 
-console.log(generateRandomString());
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
